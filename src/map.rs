faa514f6cfff0e243efd4b9595dd8476e936a1f4
Dominic <git@msrd0.de>
2025-03-11 09:54:52 +0100

    Implement `Map` for hashbrown

--- src/map.rs
+++ src/map.rs
@@ -49,3 +49,41 @@ where
         self.entry(key)
     }
 }
+
+#[cfg(feature = "hashbrown")]
+impl<'a, K, V, S> Entry<'a, K, V> for hashbrown::hash_map::Entry<'a, K, V, S>
+where
+    K: Hash,
+    S: BuildHasher,
+{
+    fn or_insert_with<F>(self, default: F) -> &'a mut V
+    where
+        F: FnOnce() -> V,
+    {
+        self.or_insert_with(default)
+    }
+}
+
+#[cfg(feature = "hashbrown")]
+impl<K, V, S> Map<K, V> for hashbrown::HashMap<K, V, S>
+where
+    K: Eq + Hash,
+    S: BuildHasher,
+{
+    type Entry<'a>
+        = hashbrown::hash_map::Entry<'a, K, V, S>
+    where
+        Self: 'a;
+
+    fn insert(&mut self, key: K, value: V) -> Option<V> {
+        self.insert(key, value)
+    }
+
+    fn remove(&mut self, key: &K) -> Option<V> {
+        self.remove(key)
+    }
+
+    fn entry(&mut self, key: K) -> Self::Entry<'_> {
+        self.entry(key)
+    }
+}
